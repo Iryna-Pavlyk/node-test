9236181371b1e019c8323563025eeb026fa5da7c
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-06-08 13:31:11 +0300

    up

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const message = 'Hello world!';
+
+const workDir = path.join(process.cwd());
+const joinSomePath = path.join(workDir, 'folder', 'text.txt');
+const parseSomePath = path.parse(
+  'D:Робочий стілІраНавчання\node-test\folder\text.txt',
+);
+console.log(message);
+console.log(joinSomePath);
+console.log(parseSomePath);
+
+const fileContent = await fs.readFile('path_to_file');
+console.log(fileContent);
+
+// const buffer = await fs.readFile('hello.txt');
+// console.log(buffer.toString('utf-8'));
