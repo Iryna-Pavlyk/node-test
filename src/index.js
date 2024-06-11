ee7a6c480c14e4392ae43c2941b0133fd6335bbb
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-06-11 23:03:10 +0300

    Les 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+// -----Lesson 1-----
 // import path from 'node:path';
 // import fs from 'node:fs/promises';
-import express from 'express';
+// -----Lesson 1-----
+// -----Lesson 2-----
+// import express from 'express';
+// import pino from 'pino-http';
+// import cors from 'cors';
+import { startServer } from './server.js';
+// -----Lesson 2-----
 
+// -----Lesson 1-----
 // const message = 'Hello world!';
 
 // const workDir = path.join(process.cwd());
@@ -97,23 +105,49 @@ import express from 'express';
 // };
 
 // accesPath();
+// -------Lesson 1-------------
 
 // -------Lesson 2-------------
-const app = express();
+// const app = express();
 
-const PORT = 3000;
+// const PORT = 3000;
 
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
+// app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
-});
+// app.use(
+//   pino({
+//     transport: {},
+//     target: 'pino-pretty',
+//   }),
+// );
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello world!',
+//   });
+// });
+
+// app.use('*', (req, res, next) => {
+//   res.status(404).json({
+//     message: 'Route not found',
+//   });
+// });
+
+// app.use((err, req, res, next) => {
+//   res.status(500).json({
+//     message: 'Smth went wrong',
+//     error: err.message,
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+startServer();
+// -------Lesson 2-------------
